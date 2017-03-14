107b48f197d33e5c45523e4fa38cb29fec8ee41a
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-03-14 14:57:11 +0100

    Fix problems with the background

--- public_html/js/modules/background.js
+++ public_html/js/modules/background.js
@@ -5,28 +5,33 @@ var Background = (function(){
 	var top = {
 		hue: 0,
 		brightness: 30,
-		saturation: 60
+		saturation: 30
 	};
 	var bottom = {
 		hue: 0,
 		brightness: 5,
-		saturation: 60
+		saturation: 30
 	};
 	var total = 1;
+	var lastGradient = undefined;
+	var lastGradientColor = undefined;
 	
 	var setTotalBricks = function(setTotal) {
 		total = setTotal;
 	};
 	
 	var update = function() {
-		top.hue = bottom.hue = Bricks.size() / total * 240;
+		top.hue = bottom.hue = Bricks.size() * 240 / total;
 	};
 	
 	var draw = function(graphics) {
-		var grd=graphics.createLinearGradient(0,0,0,graphics.canvas.height);
-		grd.addColorStop(0,'hsl(' + top.hue + ',' + top.saturation + '%,' + top.brightness + '%)');
-		grd.addColorStop(1,'hsl(' + bottom.hue + ',' + bottom.saturation + '%,' + bottom.brightness + '%)');
-		graphics.fillStyle = grd;
+		if(lastGradientColor !== top.hue) {
+			lastGradient=graphics.createLinearGradient(0,0,0,graphics.canvas.height);
+			lastGradient.addColorStop(0,'hsl(' + top.hue + ',' + top.saturation + '%,' + top.brightness + '%)');
+			lastGradient.addColorStop(1,'hsl(' + bottom.hue + ',' + bottom.saturation + '%,' + bottom.brightness + '%)');
+			lastGradientColor = top.hue;
+		}
+		graphics.fillStyle = lastGradient;
 		graphics.fillRect(0, 0, graphics.canvas.width, graphics.canvas.height);
 	};
 	
