cf18627b3a5f4cf5cd2b0d1841f209317a9a2430
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-03-15 08:35:42 +0100

    Wall bounce tweaks

--- public_html/js/modules/ai.js
+++ public_html/js/modules/ai.js
@@ -13,6 +13,14 @@ var Ai = (function() {
 		}
 		x += change;
 		targetX = Ball.getX() + Ball.getVelocityX() * ((Ball.getY() - Paddle.getY() - Paddle.getSizeY()) / -Math.abs(Ball.getVelocityY())) + Paddle.getSizeX() / (Ball.getVelocityX() < 0 ? 1.6 : -1.6);
+		while(targetX < 0 || 800 < targetX) {
+			if(targetX < 0) {
+				targetX = 0 - targetX;
+			}
+			if(targetX > 800) {
+				targetX = 800 - targetX;
+			}
+		}
 		var yDifference = Math.max(200 - (Paddle.getY() - Ball.getY() - Paddle.getSizeY()), 0) / 3;
 		var difference = x - targetX;
 		if(difference < -yDifference) {
